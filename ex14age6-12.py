44b7eb183c8ac4dbbeb4bfec5e035039d491622c
imranetaki <146876470+imranetaki@users.noreply.github.com>
2023-10-08 22:21:00 +0200

    Add files via upload

---
+++ ex14age6-12.py
@@ -0,0 +1,11 @@
+age=int(input("entrer votre age svp : "))
+if age>=6 and age<=7 :
+    print("poussin")
+elif age>=8 and age<=9 :
+    print("pupille")
+elif age>=10 and age<=11 :
+    print("minime")
+elif age >=12 :
+    print("cadet")
+else :
+    print("vous avez un age plus petit que 6")
